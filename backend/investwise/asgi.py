1856fba5e7c3616c4eb14af9c9b637172318dedd
Daniel Wanjala <134260642+MadScie254@users.noreply.github.com>
2025-03-06 11:28:16 +0300

    Add detailed comments and documentation for ASGI application configuration

--- backend/investwise/asgi.py
+++ backend/investwise/asgi.py
@@ -44,3 +44,34 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.info("ASGI application initialized with WebSocket support.")
+
+# ===========================
+# 5. Additional Notes
+# ===========================
+#
+# The ASGI application configuration in this file is similar to the WSGI application configuration in the `wsgi.py` file.
+# The key difference is the use of the `ProtocolTypeRouter` to handle different types of connections (HTTP and WebSocket).
+# The `AuthMiddlewareStack` is used to add authentication support for WebSocket connections.
+# The `URLRouter` is used to route WebSocket connections to the appropriate consumer based on the URL path.
+# The `predictor_routing.websocket_urlpatterns` includes the WebSocket URL patterns defined in the `routing.py` file of the predictor app.
+# The logging configuration at the end of the file adds a log message to indicate that the ASGI application has been initialized.
+# This logging message can be useful for monitoring and debugging ASGI-related events.
+# You can customize the logging configuration based on your specific requirements and preferences.
+# For more information on ASGI, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/
+# For more information on logging in Django, refer to the Django documentation: https://docs.djangoproject.com/en/stable/topics/logging/
+# For more information on ASGI application configuration, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/glossary.html#term-application
+# For more information on WebSocket routing in Django Channels, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/routing.html#routing
+# For more information on the ProtocolTypeRouter in Django Channels, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/routing.html#protocoltyperouter
+# For more information on the AuthMiddlewareStack in Django Channels, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/authentication.html#authmiddlewarestack
+# For more information on the URLRouter in Django Channels, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/routing.html#urlrouter
+# For more information on ASGI application initialization, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/routing.html#application
+# For more information on ASGI application configuration in Django, refer to the Django documentation: https://docs.djangoproject.com/en/stable/howto/deployment/asgi/
+# For more information on logging in Python, refer to the Python documentation: https://docs.python.org/3/library/logging.html
+# For more information on logging configuration in Django, refer to the Django documentation: https://docs.djangoproject.com/en/stable/topics/logging/
+# For more information on logging best practices, refer to the Python logging cookbook: https://docs.python.org/3/howto/logging.html#logging-cookbook
+# For more information on monitoring and debugging ASGI applications, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/debugging.html
+# For more information on WebSocket support in Django Channels, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/websockets.html
+# For more information on WebSocket consumers in Django Channels, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/consumers.html
+# For more information on WebSocket URL routing in Django Channels, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/routing.html#routing
+# For more information on WebSocket connections in Django Channels, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/websockets.html
+# For more information on WebSocket protocol support in Django Channels, refer to the Django Channels documentation: https://channels.readthedocs.io/en/stable/topics/websockets.html#protocol-support
